11afe57e88bdc9891ec9fa53af04e4adc8682d4f
Matthew Lonis <mattlonis@gmail.com>
2024-07-07 21:08:42 -0400

    Refactor

--- src/main/java/com/mrlonis/example/controller/XmlController.java
+++ src/main/java/com/mrlonis/example/controller/XmlController.java
@@ -1,13 +1,7 @@
 package com.mrlonis.example.controller;
 
-import static com.mrlonis.example.model.jackson.Constants.JACKSON_MAP;
-import static com.mrlonis.example.model.jakarta.Constants.JAKARTA_MAP;
-import static com.mrlonis.example.model.jaxb.Constants.JAXB_MAP;
-import static com.mrlonis.example.util.Constants.JAKARTA;
-import static com.mrlonis.example.util.Constants.JAXB;
-
 import com.mrlonis.example.model.BaseModel;
-import java.util.Map;
+import com.mrlonis.example.util.FetchModelUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings("rawtypes")
 @Slf4j
 public class XmlController {
-    private static final Map<String, Map<String, Map<String, Map<String, BaseModel>>>> XML_MODELS_BY_TYPE =
-            Map.of(JAXB, JAXB_MAP, JAKARTA, JAKARTA_MAP);
-
     @GetMapping("/{formatLibrary}")
     public BaseModel getModel(
             @PathVariable String formatLibrary,
@@ -30,25 +21,6 @@ public class XmlController {
             @RequestParam String dateLibrary,
             @RequestParam String zoned,
             @RequestParam(required = false) String xmlAnnotationLibrary) {
-        log.info(
-                "XmlController: getModel(): formatLibrary: {} | dateLibrary: {} | zoned: {} | | accessType: {}, xmlAnnotationLibrary: {}",
-                formatLibrary,
-                dateLibrary,
-                zoned,
-                accessType,
-                xmlAnnotationLibrary);
-        if ("jackson".equals(formatLibrary)) {
-            if ("jaxb".equals(xmlAnnotationLibrary)
-                    || "jakarta".equals(xmlAnnotationLibrary)
-                    || "jackson".equals(xmlAnnotationLibrary)) {
-                return JACKSON_MAP
-                        .get(xmlAnnotationLibrary)
-                        .get(dateLibrary)
-                        .get(zoned)
-                        .get(accessType);
-            }
-            throw new IllegalArgumentException("Invalid xmlAnnotationLibrary: " + xmlAnnotationLibrary);
-        }
-        return XML_MODELS_BY_TYPE.get(formatLibrary).get(dateLibrary).get(zoned).get(accessType);
+        return FetchModelUtil.fetchModel(formatLibrary, accessType, dateLibrary, zoned, xmlAnnotationLibrary);
     }
 }

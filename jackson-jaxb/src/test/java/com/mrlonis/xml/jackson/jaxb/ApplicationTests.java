138f8bbdb0b4ee056fc9c1c133b842af82ac19c6
Matthew Lonis <mattlonis@gmail.com>
2024-07-12 01:10:23 -0400

    Adding final jackson variants

---
+++ jackson-jaxb/src/test/java/com/mrlonis/xml/jackson/jaxb/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mrlonis.xml.jackson.jaxb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}

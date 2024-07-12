1c9f97bb90e57aeae2a4eec94d9ce3912adda4e9
Matthew Lonis <mattlonis@gmail.com>
2024-07-12 16:42:14 -0400

    Fixing tests and adding build cache

---
+++ jakarta/src/test/java/com/mrlonis/xml/jakarta/controller/XmlControllerTests.java
@@ -0,0 +1,30 @@
+package com.mrlonis.xml.jakarta.controller;
+
+import static com.mrlonis.xml.shared.TestUtils.generateTestArguments;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.mrlonis.xml.shared.BaseXmlControllerTests;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class XmlControllerTests extends BaseXmlControllerTests {
+    XmlControllerTests(@Autowired MockMvc mockMvc) {
+        super(mockMvc);
+    }
+
+    public Stream<Arguments> testArguments() {
+        return generateTestArguments(false, true, false, false, false, false);
+    }
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
